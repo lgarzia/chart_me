43640436397a5cc58fed44aae7fc202d36e64b75
Luke Garzia <lgarzia@yahoo.com>
2022-05-01 06:58:42 -0500

    first commit - to wire up github

---
+++ src/chart_me/__init__.py
@@ -0,0 +1,14 @@
+"""Entry into chart me module
+
+Add more details in time # TODO
+
+    Typical usage example
+"""
+def hello_world()->str:
+    """Dummy function to get started
+
+    Returns:
+        str: hard coded --- hello world
+    """
+    # ! This is just a simple test
+    return 'hello world'
