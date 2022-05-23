f832c1b6289315db40e8201561850793b169cfb2
Luke Garzia <lgarzia@yahoo.com>
2022-05-23 07:10:59 -0500

    set python support - add logic for typing-extensions

--- src/chart_me/chart_configs.py
+++ src/chart_me/chart_configs.py
@@ -2,7 +2,7 @@
 
 from dataclasses import dataclass
 from chart_me.data_validation_strategy import ValidateColumnStrategy, ValidateColumnStrategyDefault
-from typing import Type, TypeVar
+from typing import Type
 from chart_me.datatype_infer_strategy import InferDataTypeStrategy, InferDataTypeStrategyDefault
 from chart_me.charting_assembly_strategy import AssembleChartsStrategy, AssembleChartsStrategyDefault
 from chart_me.data_validation_strategy import ValidateColumnStrategyDefault
